fe867ce12736359c87000cb945e446d9acf33b99
gorjanmicevski <micevskigorjan2001@gmail.com>
2023-03-14 22:41:42 +0100

    structure refactor

--- bengal-cats-web-shop-api/src/main/kotlin/com/petcare/catshop/api/MainController.kt
+++ bengal-cats-web-shop-api/src/main/kotlin/com/petcare/catshop/api/MainController.kt
@@ -1,10 +1,12 @@
 package com.petcare.catshop.api
 
 import com.petcare.catshop.core.CatService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:4200"])
 class MainController(val service: CatService) {
     @GetMapping("/cats")
     fun cats() = service.findAll()
